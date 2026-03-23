4a5564ea74c5b29743150c7ffaa0cce8dd4bee6b
Rajrishi124 <sharmarishiraj5588@gmail.com>
2026-03-23 23:52:05 +0530

    file relocate

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
-const port = 8000;
 
 const app = express();
+const PORT = process.env.PORT ||5000;
 
 app.get("/", (req, res) => {
     res.json({ message: "Exam Notes AI Backend Running Successfully 🚀 " })
 })
 
-app.listen(port, () => {
-    console.log(`✅ Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`✅ Server is running on PORT ${PORT}`);
 })
